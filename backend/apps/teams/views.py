44d2dd34a0ff52748bd4df00702b0e47a267253c
Matthew <matthew@hmongvaj.de>
2026-03-21 01:09:11 -0500

    Modified teams/views.py to reflect current state of Team/serializer.py

--- backend/apps/teams/views.py
+++ backend/apps/teams/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Team, Coach
+from .serializers import TeamSerializer, CoachSerializer
 
 # Create your views here.
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class CoachViewSet(viewsets.ModelViewSet):
+    queryset = Coach.objects.all()
+    serializer_class = CoachSerializer
