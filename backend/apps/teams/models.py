ab5e57402ab12ae29a34824fca3c626bef1ac017
Matthew <matthew@hmongvaj.de>
2026-03-20 23:33:50 -0500

    Added class RoleChoices in teams/models.py for Coach model.  Edited relevant field to use RoleChoices

--- backend/apps/teams/models.py
+++ backend/apps/teams/models.py
@@ -21,11 +21,17 @@ class Team(models.Model):
         return self.team_name
 
 class Coach(models.Model):
+    class RoleChoices(models.TextChoices):
+        HEAD_COACH = "Head Coach", "Head Coach"
+        OFFENSIVE_COORDINATOR = "Offensive Coordinator", "Offensive Coordinator"
+        DEFENSIVE_COORDINATOR = "Defensive Coordinator", "Defensive Coordinator"
+        SPECIAL_TEAMS_COORDINATOR = "Special Teams Coordinator", "Special Teams Coordinator"
+
     coach_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    role = models.CharField(max_length=50)  # Head Coach, OC, DC, etc
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='coaches')
+    last_name = models.CharField(max_length=50, db_index=True)
+    role = models.CharField(max_length=50, choices=RoleChoices.choices)
+    team = models.ForeignKey(Team, on_delete=models.PROTECT, related_name='coaches')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.role})"
