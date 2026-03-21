f2808e28a1958419fe1317bc58df880d04184136
Matthew <matthew@hmongvaj.de>
2026-03-21 15:24:29 -0500

    Finalized seasons/admin.py to reflect current state of seasons/models.py

--- backend/apps/seasons/admin.py
+++ backend/apps/seasons/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Season, TeamSeason
 
 # Register your models here.
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('season_id', 'year')
+    search_fields = ('year',)
+
+
+@admin.register(TeamSeason)
+class TeamSeasonAdmin(admin.ModelAdmin):
+    list_display = ('team_season_id', 'team', 'season', 'conference', 'division')
+    list_filter = ('conference', 'division', 'season')
+    search_fields = ('team__team_name', 'team__city', 'season__year')
