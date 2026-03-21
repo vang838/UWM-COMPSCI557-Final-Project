054917442c8784ea643c03d4cc7904713d41614d
Matthew <matthew@hmongvaj.de>
2026-03-21 02:27:36 -0500

    Registered team and coach model in admin

--- backend/apps/teams/admin.py
+++ backend/apps/teams/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Team, Coach
 
 # Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('team_id', 'team_name', 'conference', 'division')
+    list_filter = ('team_name',)
+    search_fields = ('conference', 'division')
+
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ('coach_id', 'first_name', 'last_name', 'role', 'team')
+    list_filter = ('role', 'team')
+    search_fields = ('first_name', 'last_name')
