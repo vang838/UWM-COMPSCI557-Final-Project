70e341fa35bac1b787e15edb7c847b05a3f439a1
Matthew <matthew@hmongvaj.de>
2026-03-21 02:27:21 -0500

    Registered player model in admin

--- backend/apps/players/admin.py
+++ backend/apps/players/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Player
 
 # Register your models here.
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('player_id', 'first_name', 'last_name', 'position', 'team', 'is_active')
+    list_filter = ('position', 'is_active', 'team')
+    search_fields = ('first_name', 'last_name')
