4d0d83377b12cbf59a6005f7575c8d3484a7769f
Matthew <matthew@hmongvaj.de>
2026-03-21 00:14:11 -0500

    added db_index=True to Players.last_name field

--- backend/apps/players/models.py
+++ backend/apps/players/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.teams.models import Team
+from django.core.validators import MinValueValidator
 
 # Create your models here
 class Player(models.Model):
@@ -18,12 +19,12 @@ class Player(models.Model):
 
     player_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, db_index=True)
     position = models.CharField(max_length=2,choices=PositionChoices.choices)
     team = models.ForeignKey(Team, on_delete=models.PROTECT, related_name='players')
     date_of_birth = models.DateField(null=True, blank=True)
-    height = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True)
-    weight = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True)
+    height = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True, validators=[MinValueValidator(0.0)])
+    weight = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True, validators=[MinValueValidator(0.0)])
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
