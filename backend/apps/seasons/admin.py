97175b66c5a9a6412decce6f728a66204d248352
vang838 <vang838@uwm.edu>
2026-04-09 14:13:19 -0500

    feat: completed seasons/admin.py

--- backend/apps/seasons/admin.py
+++ backend/apps/seasons/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Season
 
 # Register your models here.
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ("season_id", "year",)
+    list_filter = ("year",)
+    search_fields = ("year",)
+    ordering = ("-year",)
