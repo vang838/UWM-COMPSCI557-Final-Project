8cf143808771942c66471b8b0d04e28fe63a01b8
vang838 <vang838@uwm.edu>
2026-04-09 13:54:15 -0500

    fix: removed boolean field is_active from seasons/models.py

--- backend/apps/seasons/models.py
+++ backend/apps/seasons/models.py
@@ -1,9 +1,13 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
 class Season(models.Model):
     season_id = models.AutoField(primary_key=True)
-    year = models.IntegerField(unique=True)
+    year = models.PositiveIntegerField(unique=True, validators=[MinValueValidator(1920), MaxValueValidator(2030)])
+
+    class Meta:
+        ordering = ["-year"]
 
     def __str__(self):
         return str(self.year)
