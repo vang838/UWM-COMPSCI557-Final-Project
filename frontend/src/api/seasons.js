9c7566b965005329a03415628ae46627553bffdb
Matthew <matthew@hmongvaj.de>
2026-04-26 14:44:33 -0500

    feat: add api for data fetching and crud operations on seasons

---
+++ frontend/src/api/seasons.js
@@ -0,0 +1,9 @@
+import apiClient from './index/';
+
+export const seasonAPI = {
+    getAllSeasons: () => apiClient.get('/seasons/'),
+    getSeasonByID: (id) => apiClient.get(`/seasons/${id}/`),
+    createSeason: (seasonData) => apiClient.post('seasons/', seasonData),
+    updateSeason: (id, seasonData) => apiClient.put(`/seasons/${id}/`, seasonData),
+    deleteSeason: (id) => apiClient.delete(`/seasons/${id}/`),
+}
