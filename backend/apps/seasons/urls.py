5522dbffc45175640adea734aabb699277364493
Matthew <matthew@hmongvaj.de>
2026-04-29 15:13:51 -0500

    urls: register url for historical tracking api

--- backend/apps/seasons/urls.py
+++ backend/apps/seasons/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.seasons.views import SeasonViewSet
+from apps.seasons.views import SeasonViewSet, TeamSeasonViewSet
 
 router = DefaultRouter()
 router.register(r"seasons", SeasonViewSet, basename="season")
+router.register(r"team-seasons", TeamSeasonViewSet, basename="team-season")
 
 urlpatterns = [
     path("", include(router.urls)),
