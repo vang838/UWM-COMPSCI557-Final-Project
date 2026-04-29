525d2e496aaf95df5e5e68fd7b64ce796ad109ce
Matthew <matthew@hmongvaj.de>
2026-04-29 02:42:15 -0500

    feat: added serializer for team app

---
+++ backend/apps/teams/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from apps.teams.models import Team, Coach
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = (
+            "team_id",
+            "city",
+            "state",
+            "team_name",
+            "display_name",
+            "conference",
+            "division",
+            "abbreviation",
+            "primary_color",
+            "secondary_color",
+            "text_color",
+        )
+
+    def get_display_name(self, obj):
+        return f"{obj.city} {obj.team_name}".strip() or obj.team_name
+
+class CoachSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coach
+        fields = [
+            "coach_id",
+            "first_name",
+            "last_name",
+            "role",
+            "team",
+        ]
