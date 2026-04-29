8c00a10c45050e0ebf3b95be527b523025a4d700
Matthew <matthew@hmongvaj.de>
2026-04-29 00:47:00 -0500

    fix: typo

--- frontend/src/api/seasons.js
+++ frontend/src/api/seasons.js
@@ -1,9 +1,9 @@
-import apiClient from './index/';
+import apiClient from './index';
 
 export const seasonAPI = {
     getAllSeasons: () => apiClient.get('/seasons/'),
     getSeasonByID: (id) => apiClient.get(`/seasons/${id}/`),
-    createSeason: (seasonData) => apiClient.post('seasons/', seasonData),
+    createSeason: (seasonData) => apiClient.post('/seasons/', seasonData),
     updateSeason: (id, seasonData) => apiClient.put(`/seasons/${id}/`, seasonData),
     deleteSeason: (id) => apiClient.delete(`/seasons/${id}/`),
 }
