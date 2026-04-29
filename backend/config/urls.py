b24365616e8beb796567e0e34bc2c200a452424f
Matthew <matthew@hmongvaj.de>
2026-04-29 01:19:23 -0500

    fix: added urls for seasons app

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.players.urls')),
-    # path('api/', include('apps.teams.urls')),
+    path('api/', include('apps.seasons.urls')),
     path('api/auth/', include('apps.users.urls')),
 ]
