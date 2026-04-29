e2d17a72ba659c4cc6eea88d12ec89930d410992
Matthew <matthew@hmongvaj.de>
2026-04-29 02:22:39 -0500

    no idea what's different here besides more imports.

--- backend/apps/teams/views.py
+++ backend/apps/teams/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Team
+from ..stats.models import PlayerSeasonStat, TeamSeasonStat
 
 # Create your views here.
