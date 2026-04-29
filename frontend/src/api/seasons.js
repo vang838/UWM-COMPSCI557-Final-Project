73793e15302e17d1e60b98cb50856e13738bfa9e
Matthew <matthew@hmongvaj.de>
2026-04-29 14:36:05 -0500

    fix: change update to use patch

--- frontend/src/api/seasons.js
+++ frontend/src/api/seasons.js
@@ -4,6 +4,6 @@ export const seasonAPI = {
     getAllSeasons: () => apiClient.get('/seasons/'),
     getSeasonByID: (id) => apiClient.get(`/seasons/${id}/`),
     createSeason: (seasonData) => apiClient.post('/seasons/', seasonData),
-    updateSeason: (id, seasonData) => apiClient.put(`/seasons/${id}/`, seasonData),
+    updateSeason: (id, seasonData) => apiClient.patch(`/seasons/${id}/`, seasonData),
     deleteSeason: (id) => apiClient.delete(`/seasons/${id}/`),
 }
