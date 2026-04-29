df2affb8d23690996c3a4130761b8678ca780749
Matthew <matthew@hmongvaj.de>
2026-04-29 15:33:01 -0500

    django: added playerSeasonRoster url

--- backend/apps/seasons/urls.py
+++ backend/apps/seasons/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.seasons.views import SeasonViewSet, TeamSeasonViewSet
+from apps.seasons.views import SeasonViewSet, TeamSeasonViewSet, PlayerSeasonRosterViewSet
 
 router = DefaultRouter()
 router.register(r"seasons", SeasonViewSet, basename="season")
 router.register(r"team-seasons", TeamSeasonViewSet, basename="team-season")
-
+router.register(r"player-season-rosters", PlayerSeasonRosterViewSet, basename="player-season-roster",)
 urlpatterns = [
     path("", include(router.urls)),
 ]
