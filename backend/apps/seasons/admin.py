1c326b4722b9e35d17e309ecca1f8fe081abe912
Matthew <matthew@hmongvaj.de>
2026-04-29 15:15:25 -0500

    admin: register in django admin

--- backend/apps/seasons/admin.py
+++ backend/apps/seasons/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Season
+from .models import Season, TeamSeason
 
 # Register your models here.
 @admin.register(Season)
@@ -8,3 +8,21 @@ class SeasonAdmin(admin.ModelAdmin):
     list_filter = ("year",)
     search_fields = ("year",)
     ordering = ("-year",)
+
+@admin.register(TeamSeason)
+class TeamSeasonAdmin(admin.ModelAdmin):
+    list_display = (
+        "team_season_id",
+        "team",
+        "season",
+        "conference",
+        "division",
+    )
+    list_filter = ("season", "conference", "division")
+    search_fields = (
+        "team__team_name",
+        "team__city",
+        "team__abbreviation",
+        "season__year",
+    )
+    ordering = ("-season__year", "conference", "division", "team__team_name")
