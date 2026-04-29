503dc338fce86c2fccc120955f2aa1175266bf25
Matthew <matthew@hmongvaj.de>
2026-04-29 01:12:14 -0500

    feat: update views for seasons app

--- backend/apps/seasons/views.py
+++ backend/apps/seasons/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from apps.seasons.models import Season
+from apps.seasons.serializers import SeasonSerializer
 
 # Create your views here.
+class SeasonViewSet(viewsets.ModelViewSet):
+    queryset = Season.objects.all().order_by("year")
+    serializer_class = SeasonSerializer
+    lookup_field = "season_id"
