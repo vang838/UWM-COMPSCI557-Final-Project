95f946887f98398f01cc239b0e03b4aee66bc747
Matthew <matthew@hmongvaj.de>
2026-05-02 17:39:21 -0500

    optimization: changed localhost to 127.0.0.1 as localhost seems to be 1.5seconds behind 127.0.0.1

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // default config of instance
 const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api",
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000/api",
     timeout: 30000,
     headers: {
         "Content-Type": "application/json",
